cdcbb3bc322ecc07078d46b387d71bbc9aa2276d
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-07-08 19:42:17 -0400

    add contact form functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,11 @@ export default function Footer() {
   return (
     <div className="footer">
       
+      <img src="./img/Github-Logo.png" href="https://github.com/hfang821" class="card-img-top" alt="Github" id="git" />
+
+      <img src="./img/Linkedln.png" href="https://www.linkedin.com/in/haoyufang" class="card-img-top" alt="LinkedIn" id="linkedln" />
+
     </div>
+
   )
 }
--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,13 +1,79 @@
 import React from 'react';
 import "./About.css"
 
-export default function About() {
+export default function About(currentPage, handlePageChange) {
   return (
     <div className="about" id="coffee">
       <h1>About Page</h1>
-      <h2>Hello, my name is <a href="https://github.com/hfang821?tab=repositories" class="colored-font">Kevin Fang</a>.      
-        Check out my page for my <a href="https://github.com/hfang821?tab=repositories" class="colored-font">latest projects</a>.  
-        Or you can reach out to me directly through my <a href="#contact-myself" class="colored-font">Contact page</a>. </h2>
+      <div id="pictures">
+      <img src="./img/profilepic.jpg" alt="profile" width="" height="200" />
+      <img src="./img/powerlifting.jpg" alt="profile" width="" height="200" />
+      </div>
+      <div id="aboutText">
+      <h1>My name is Kevin Fang </h1> 
+      <h1>A Full-Stack Web developer from Toronto, Ontario. ✨</h1>
+      <p>I’m currently performing Full-Stack Web Development using HTML, CSS and JavaScript, Node.js and their frameworks & libraries! 🌱</p> 
+      <p>*Fun fact: Once a chemical Engineer, now a full-time Developer, and a part-time Powerlifter. ⚡ </p> 
+      </div>
+      <h1>Skills</h1>
+
+      <div class="row">
+      <div class="col-sm-3">
+        <div id="frontend" class="card">
+          <img src="./img/frontend.webp" class="card-img-top" alt="..." height="200" width="50" />
+          <div class="card-body">
+              <h5 class="card-title">Frontend Development Stack</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">HTML</li>
+              <li class="list-group-item">JavaScript</li>
+              <li class="list-group-item">CSS</li>
+              <li class="list-group-item">React</li>
+          </ul>    
+        </div>
+        </div>
+
+        <div class="col-sm-3">
+        <div id="backend" class="card">
+        <img src="./img/backend.webp" class="card-img-top" alt="..." height="200" width="50" />
+          <div class="card-body">
+              <h5 class="card-title">Backend Development Stack</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">Express.js</li>
+              <li class="list-group-item">Node.js</li>
+          </ul>
+        </div>
+        </div>
+
+        <div class="col-sm-3">
+        <div id="database" class="card">
+        <img src="./img/database.png" class="card-img-top" alt="..." height="200" width="50" />
+          <div class="card-body">
+              <h5 class="card-title">Database & Storage</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">SQL</li>
+              <li class="list-group-item">MongoDB</li>
+              <li class="list-group-item">ORM & ODM (Sequelize, Mongoose)</li>
+          </ul>
+        </div>
+        </div>
+
+        <div class="col-sm-3">
+        <div id="control" class="card">
+        <img src="./img/control.png" class="card-img-top" alt="..." height="200" width="50" />
+          <div class="card-body">
+              <h5 class="card-title">Control & Tests</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">Git</li>
+              <li class="list-group-item">Jest</li>
+              <li class="list-group-item">npm</li>
+          </ul>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
